8ecaf5dc86f1799530f8d25eec88421e085d15e0
Bogdan Hener <bogdan@heners94.com>
2020-06-06 21:14:59 +0300

    Updates

---
+++ precache-manifest.5ce5f0ec393013ce504bb13182a62fe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73a73082b80f238f7d80d6b4345257b",
+    "url": "/React-organizer/index.html"
+  },
+  {
+    "revision": "3f6905de4173ddf20b4f",
+    "url": "/React-organizer/static/js/2.48d432e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-organizer/static/js/2.48d432e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd1f74db5551854df608",
+    "url": "/React-organizer/static/js/main.d8aef612.chunk.js"
+  },
+  {
+    "revision": "8ccfa787331ea24b2d26",
+    "url": "/React-organizer/static/js/runtime-main.f55c9086.js"
+  }
+]);
