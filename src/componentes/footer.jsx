7dbe8bd3540ec4ac3397924a4471b647d955783f
Bogdan Hener <bogdan@heners94.com>
2020-06-06 14:12:56 +0300

    Initial Commit

---
+++ src/componentes/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer(){
+    
+    let currentYear =new Date().getFullYear();
+
+    return (
+        <footer>
+        <p> bogdanhener © {currentYear} </p>
+    </footer>
+    );
+   
+}
+
+export default Footer;
---
+++ src/componentes/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HighlightTwoToneIcon from '@material-ui/icons/HighlightTwoTone';
+
+function Header(){
+    return (
+    <header>
+      <h1><HighlightTwoToneIcon /> Keeper</h1>
+    </header>
+);
+    
+}
+
+export default Header;
